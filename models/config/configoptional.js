e3a24f6733074fadf54b0697be21fc8f0e1975a2
nfxpnk <nfxpnk@gmail.com>
2023-01-22 04:02:25 +0200

    config optional

--- models/config/configoptional.js
+++ models/config/configoptional.js
@@ -13,8 +13,6 @@ function getOptionalBaseConfigs(config) {
     atlasConfig.excludedCssFiles = new RegExp(config.excludedCssFiles || '.^');
     atlasConfig.excludedSassFiles = new RegExp(config.excludedSassFiles || '.^');
 
-    const copyInternalAssets = config.copyInternalAssets;
-    atlasConfig.copyInternalAssets = copyInternalAssets !== undefined ? copyInternalAssets : true;
     atlasConfig.internalAssetsPath = path.join(__dirname, '../../assets');
 
     atlasConfig.componentPrefixes = getComponentsPrefix(config);
