450080fa6a7b899bf5e5e2a24822edbf9c9f080c
nfxpnk <nfxpnk@gmail.com>
2023-02-26 14:09:49 +0200

    configmandatory

--- models/config/configmandatory.js
+++ models/config/configmandatory.js
@@ -6,53 +6,62 @@ const log = require('fancy-log');
 const c = require('ansi-colors');
 const projectRoot = process.cwd();
 
-const absPath = relPath => path.join(projectRoot, relPath, '/');
-const isPathReachable = (destination, name) => {
+function absPath(relPath) {
+    return path.join(projectRoot, relPath, '/');
+}
+
+function isPathReachable(destination, name) {
     if (!destination) {
         log(c.red('Error: ') + '"' + name + '" not defined. This field is mandatory');
+
         return false;
-    } else if (!fs.existsSync(absPath(destination))) {
-        log(c.red('Error: ') + '"' + name + '" (' + destination + ') in config unavailable or unreadable. ' +
-            'Please check this path in config');
-        return false;
-    } else {
-        return true;
     }
-};
+
+    if (fs.existsSync(destination) === false && fs.existsSync(absPath(destination)) === false) {
+        log(c.red('Error: ') +
+            '"' + name + '" (' + destination + ', ' + absPath(destination) + ')' +
+            ' in config unavailable or unreadable. ' +
+            'Please check this path in config');
+
+        return false;
+    }
+
+    return true;
+}
 
 function getMandatoryBaseConfig(config) {
+    const corruptedConfig = { isCorrupted: true };
+
     if (!isPathReachable(config.guideSrc, 'guideSrc') ||
         !isPathReachable(config.cssSrc, 'cssSrc')) {
-        return { isCorrupted: true }; // return with corrupted config if we don't have critical info
+        return corruptedConfig; // return with corrupted config if we don't have critical info
     }
 
-    let atlasConfig = {};
+    if (!config.guideDest) {
+        log(c.red('Error: ') + '"guideDest" not defined. This field is mandatory');
+        return corruptedConfig;
+    }
 
     // Process mandatory configs
-    atlasConfig.guideSrc = absPath(config.guideSrc);
-    if (fs.existsSync(config.guideSrc)) {
-        atlasConfig.guideSrc = config.guideSrc;
-    }
-
-    atlasConfig.cssSrc = absPath(config.cssSrc);
+    const atlasConfig = {
+        guideSrc: fs.existsSync(config.guideSrc) ? config.guideSrc : absPath(config.guideSrc),
+        cssSrc: fs.existsSync(config.cssSrc) ? config.cssSrc : absPath(config.cssSrc),
+        guideDest: fs.existsSync(config.guideDest) ? config.guideDest : absPath(config.guideDest)
+    };
 
     // Check and create destination directory if needed
     const createDestination = config.createDestFolder || false;
-    if (!config.guideDest) {
-        log(c.red('Error: ') + '"guideDest" not defined. This field is mandatory');
-        return { isCorrupted: true };
-    }
-    if (!fs.existsSync(absPath(config.guideDest))) {
+
+    if (fs.existsSync(atlasConfig.guideDest) === false) {
         if (createDestination) {
-            fs.mkdirSync(path.join(projectRoot, config.guideDest));
-            log(c.yellow('Warning: ') + '"guideDest": ' + config.guideDest + ' directory created');
+            fs.mkdirSync(atlasConfig.guideDest);
+            log(c.yellow('Warning: ') + '"guideDest": ' + atlasConfig.guideDest + ' directory created');
         } else {
-            log(c.red('Error: ') + '"guideDest" (' + config.guideDest + ') in config unavailable or unreadable. ' +
+            log(c.red('Error: ') + '"guideDest" (' + atlasConfig.guideDest + ') in config unavailable or unreadable. ' +
                 'Please check this path in config');
-            return { isCorrupted: true };
+            return corruptedConfig;
         }
     }
-    atlasConfig.guideDest = absPath(config.guideDest);
 
     console.log(atlasConfig, config);
 
